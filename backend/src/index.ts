8e89e811de2a950e5f6f929b31e8be92c7141709
Dylan <inutuzumab@gmail.com>
2024-12-11 14:38:00 +1100

    backend boilerplate

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { errorHandler } from './middleware/error';
+
+const app = express();
+
+app.use(errorHandler);
+
+app.get('/', (req, res, next) => {
+    console.log("Hello World!")
+});
+
+app.listen(3000, () => {
+    console.log(`✨ Server started on port 8080! ✨`);
+  });
